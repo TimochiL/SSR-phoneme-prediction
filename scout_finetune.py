3e802b719a387b1a1cf7ddfda39f3c0b7a028322
Timothy Liu <timothy.l.6uy@gmail.com>
2025-10-18 13:45:13 -0500

    feat: created finetuning structure - Modified: scout_finetune.py - Modified: README.md to include PyTorch instructions

--- scout_finetune.py
+++ scout_finetune.py
@@ -1,2 +1,16 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoProcessor, Llama4ForConditionalGeneration
+import torch
+
+# Supported filetypes: CSV, JSON, Parquet, txt, PNG, JPEG, WAV, MP3
+dataset_dir = "data/"
+dataset_path = dataset_dir + ""
+dataset = load_dataset(dataset_path)
+
+model_id = "meta-llama/Llama-4-Scout-17B-16E-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+def tokenize(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+dataset = dataset.map(tokenize, batched=True)
